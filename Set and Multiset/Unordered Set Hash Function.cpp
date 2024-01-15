871b71ba5207b8e44c989bf18c8afa1a6c6a4cd8
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-01-15 19:25:28 +0530

    Create Unordered Set Hash Function.cpp

---
+++ Set and Multiset/Unordered Set Hash Function.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+// unordered_set::hash_function
+#include <iostream>
+#include <unordered_set>
+#include <string>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+//Complete the function
+long int HashMe (unordered_set<string> myset, string str){
+   auto fn = myset.hash_function();
+   return fn(str);
+    
+}
+
+
+//{ Driver Code Starts.
+
+int main ()
+{
+
+  int t;
+  cin>>t;
+  
+  while(t--){
+
+  string str;
+  cin>>str;
+
+  unordered_set<string> myset;
+
+  cout << HashMe(myset, str) << endl;
+
+}
+ 
+  return 0;
+}
+
+// } Driver Code Ends
