0d983e643aaa02da647f5a0a1d665e43330d6a50
Ojas Arora <127867874+Ojas-Arora@users.noreply.github.com>
2024-01-19 23:16:39 +0530

    Create Size of an Array.cpp

---
+++ Array/Size of an Array.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        
+// } Driver Code Ends
+// User function Template for C++
+
+// a[] : declared and initialized above
+
+// complete the code here.
+int size =n ;
+
+// driver code prints size
+
+//{ Driver Code Starts.
+
+        cout << size << "\n";
+    }
+}
+
+// } Driver Code Ends
